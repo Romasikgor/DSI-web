b1b9cf411ca0374b514f982aafa48b18ee57881f
Roma <romangorgota6@gmail.com>
2025-11-08 01:21:21 +0200

    додано скрипт при наведені на кнопку

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-alert("Вітаю на нашому сайті!");
+/* alert("Вітаю на нашому сайті!");
 let years = prompt("Скільки вам років?", 18);
 if (years >= 18) {
     let zgoda = confirm("Чи погоджуєтеся ви з вимогами сайту?");
@@ -10,3 +10,19 @@ if (years >= 18) {
 } else {
     alert("На жаль вам не можна перебувати на цьому сайті :(")
 }
+
+ */
+
+let button = document.querySelector("button");
+
+button.onmouseover = function () {
+    button.style.backgroundColor = "#c7a9a9ff"
+}
+
+button.onmouseout = function () {
+    button.style.backgroundColor = "#EE4248"
+}
+
+button.onclick = function () {
+    button.style.backgroundColor = "#996363ff";
+};
